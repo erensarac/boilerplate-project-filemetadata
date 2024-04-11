c8d81c1ac993091315ee16b69a44724f01ac66e4
Eren S <esarac@outlook.com.tr>
2024-04-11 13:50:08 +0000

    add /api/fileanalyse handler for file metadata response

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-var app = express();
+const app = express();
+const upload = multer({ dest: './public/uploads/' })
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +14,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'), async (req, res) => {
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size })
+})
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, () => {
   console.log('Your app is listening on port ' + port)
 });
